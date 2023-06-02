cf882249368b81277a3c5da9aaa2bf084d4b2144
alexarnoldy <alexarnoldy@gmail.com>
2023-06-02 16:43:45 -0700

    updated for better labeling across events

--- elemental-demo/script/updater.sh
+++ elemental-demo/script/updater.sh
@@ -6,12 +6,38 @@ export KUBECONFIG=/home/opensuse/.kube/elemental.susealliances.com-local
 while :
 do
 
-kubectl -n fleet-default get machineinventory -l arcade-location=null --no-headers -o custom-columns=N:.metadata.name > machines
+	
+## This command finds all of the edge nodes that have been provisioned, but do not yet have serial numbers
+kubectl -n fleet-default get machineinventory -l serialNumber=Not-Specified --no-headers -o custom-columns=N:.metadata.name > machines
 
 COUNT=1
-#COUNT=$(wc -l machines | awk '{print$1}')
 
 END=$(expr $(wc -l machines | awk '{print$1}') + 1)
+
+while [ ${COUNT} -lt ${END} ] 
+do 
+	echo ${COUNT} 
+	LINE=($(head "-$COUNT" inventory | tail -1))
+	MACHINE=($(head "-$COUNT" machines | tail -1))
+	echo ${MACHINE}
+	SN=$(head "-$COUNT" machines | tail -1 | awk -F- '{print$6}')
+	# serialNumber
+	kubectl -n fleet-default label machineinventory ${MACHINE} --overwrite serialNumber=${SN}
+
+	((COUNT++)) 
+done
+
+## This command finds all of the edge nodes that been powered on at the edge location
+# kubectl -n fleet-default get machineinventory -l arcade-location=null -o custom-columns=:.metadata.name,:.status.plan.appliedChecksum | awk '/\<none\>/ {print$1}' > machines
+
+kubectl -n fleet-default get machineinventory -l arcade-location=null -o custom-columns=:.metadata.name,:.status.plan.appliedChecksum | grep -v \<none\> | awk '{print$1}' > machines
+
+COUNT=1
+
+END=$(expr $(grep -v '^\s' machines | wc -l | awk '{print$1}') + 1)
+
+echo ${COUNT}
+echo ${END}
 
 while [ ${COUNT} -lt ${END} ] 
 do 
@@ -26,16 +52,10 @@ do
 	mv tmp-inventory inventory
 	# game-cabinet-floor-coordiantes
 	kubectl -n fleet-default label machineinventory ${MACHINE} --overwrite game-cabinet-floor-coordiantes=${LINE[1]}
-	#echo "kubectl -n fleet-default label machineinventory ${MACHINE} --overwrite game-cabinet-floor-coordiantes=${LINE[0]}"
 	# arcade-location
 	kubectl -n fleet-default label machineinventory ${MACHINE} --overwrite arcade-location=${LINE[2]}
-	#echo "kubectl -n fleet-default label machineinventory ${MACHINE} --overwrite arcade-location=${LINE[1]}"
 	# create-cluster-selector
 	kubectl -n fleet-default label machineinventory ${MACHINE} --overwrite create-cluster-selector=${LINE[3]}
-	#echo "kubectl -n fleet-default label machineinventory ${MACHINE} --overwrite create-cluster-selector=${LINE[2]}"
-	# serialNumber
-	kubectl -n fleet-default label machineinventory ${MACHINE} --overwrite serialNumber=${SN}
-	#echo "kubectl -n fleet-default label machineinventory ${MACHINE} --overwrite serialNumber=${SN}"
 
 	((COUNT++)) 
 done
